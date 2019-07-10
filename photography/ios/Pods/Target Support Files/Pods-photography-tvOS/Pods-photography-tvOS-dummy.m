b2ec68887ede495961cd700de126c13806d06847
jielong <jie1ong@icloud.com>
2019-07-10 11:07:39 +0800

    init

---
+++ photography/ios/Pods/Target Support Files/Pods-photography-tvOS/Pods-photography-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photography_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_photography_tvOS
+@end
---
+++ photography/ios/Pods/Target Support Files/Pods-photography-tvOSTests/Pods-photography-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photography_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_photography_tvOSTests
+@end
